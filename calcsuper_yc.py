7a6e534f9cde03c8c382ce0b826a277061f329d9
prototyped1 <garywen0@gmail.com>
2025-09-01 18:02:46 +1000

    final

--- calcsuper_yc.py
+++ calcsuper_yc.py
@@ -4,18 +4,21 @@ import sys
 import os
 import pandas as pd
 
+SUPER_RATE = 0.095
 """
 This module handles inputs and outputs of the files
 """
-def read_file(path = r'/data/', filename = r'Sample Super Data.xlsx'):
+def read_file(path, filename):
     if not os.path.isdir(path):
         print("Error: path does not exist.")
+        print(path)
         sys.exit()
 
     full_path = os.path.join(path, filename)
     
     if not os.path.isfile(full_path):
         print("Error: file does not exist.")
+        print(full_path)
         sys.exit()
 
     print(f"Reading file from: {full_path}")
@@ -51,8 +54,8 @@ def main():
     df_final = pd.merge(df_ote, df_disbursed, how='outer', on=['employee_code','year','quarter'])
     df_final = df_final.fillna(0)
     df_final.loc[:,'Variance'] = df_final['Super Payable'] - df_final['Total Disbursed']
-    df_final.to_csv(os.path.join(path,'results.csv', index=False))
-    
+    df_final.to_csv(os.path.join(path,'results.csv'), index=False)
+
     print(f"Results saved to {os.path.join(path,'results.csv')}")
     print("Done!")
 """
@@ -68,13 +71,13 @@ def calc_ote(df_payslip, df_paycodes):
     
     df_payslip_ote.loc[:,'date'] = pd.to_datetime(df_payslip_ote['end'])
     df_payslip_ote.loc[:,'year'] = df_payslip_ote['date'].dt.year
-    df_payslip_ote.loc[:,'quarter'] = df_payslip_ote['date'].dt.month%12//3+1
+    df_payslip_ote.loc[:,'quarter'] = (df_payslip_ote['date'].dt.month-1)%12//3+1
 
-
+    df_payslip_ote.to_csv('temp_ote.csv', index=False)
     df_payslip_ote_sum = df_payslip_ote.groupby(['employee_code','year','quarter']).agg({'amount':'sum'}).reset_index()
     df_payslip_ote_sum = df_payslip_ote_sum.rename(columns={'amount':'Total OTE'})
 
-    df_payslip_ote_sum.loc[:,'Super Payable'] = df_payslip_ote_sum['Total OTE'] * 0.095
+    df_payslip_ote_sum.loc[:,'Super Payable'] = df_payslip_ote_sum['Total OTE'] * SUPER_RATE
 
     return df_payslip_ote_sum
 
@@ -84,7 +87,7 @@ This module allows the calculation of the disbursements
 def calc_disb(df_disburse):
     df_disburse.loc[:,'date'] = pd.to_datetime(df_disburse['payment_made'])
     df_disburse.loc[:,'year'] = df_disburse['date'].dt.year
-    df_disburse.loc[:,'quarter'] = df_disburse['date'].dt.month%12//3+1
+    df_disburse.loc[:,'quarter'] = (df_disburse['date'].dt.month-1)%12//3+1
 
     df_disburse_sum = df_disburse.groupby(['employee_code','year','quarter']).agg({'sgc_amount':'sum'}).reset_index()
     df_disburse_sum = df_disburse_sum.rename(columns={'sgc_amount':'Total Disbursed'})
